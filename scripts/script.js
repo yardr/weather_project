53a1e337f0739a93eeb5e7319bddbd6b34a4b311
yardr <webtraveller22@gmail.com>
2015-12-16 21:49:48 +0000

    chnages

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,17 @@
 $(function() {
-    $.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q=Melbourne' + 
-              '&units=metric&cnt=3&appid=2de143494c0b295cca9337e1e96b00e0', 
+   $.getJSON(
+        'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
+        + 'Lviv' + '&APPID=93423cd537d8b5132f4cff5001224210&cnt=3&units=metric' + '&lang=' + 'eng' + '&callback=?', 
         function(data) {
-            $('#tempToday').html(data.list[0].temp.day);
-            $('#tempTomorrow').html(data.list[1].temp.day);
-            $('#tempAfterTomorrow').html(data.list[2].temp.day);
+            $('#tempToday').html(Math.round(data.list[0].temp.day)+' &deg;C');
+            $('#tempTomorrow').html(Math.round(data.list[1].temp.day)+' &deg;C');
+            $('#tempAfterTomorrow').html(Math.round(data.list[2].temp.day)+' &deg;C');
             $('#pressureToday').html(data.list[0].pressure);
             $('#pressureTomorrow').html(data.list[1].pressure);
             $('#pressureAfterTomorrow').html(data.list[2].pressure);
-            $('#iconToday').html('<img src="/icons/'+ data.list[0].weather[0].icon + '.png" alt="Weather icon">');
-            $('#iconTomorrow').html('<img src="/icons/'+ data.list[1].weather[0].icon + '.png" alt="Weather icon">');
-            $('#iconAfterTomorrow').html('<img src="/icons/'+ data.list[2].weather[0].icon + '.png" alt="Weather icon">');
+            $('#iconToday').html('<img src="images/'+ data.list[0].weather[0].icon + '.png" alt="Weather icon">');
+            $('#iconTomorrow').html('<img src="images/'+ data.list[1].weather[0].icon + '.png" alt="Weather icon">');
+            $('#iconAfterTomorrow').html('<img src="images/'+ data.list[2].weather[0].icon + '.png" alt="Weather icon">');
         }
     );
 });
