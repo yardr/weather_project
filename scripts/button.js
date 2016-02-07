74f727e357de96be2f4d353e2e23b8e1589fb718
yardr <webtraveller22@gmail.com>
2016-02-07 21:16:29 +0000

    new script added

---
+++ scripts/button.js
@@ -0,0 +1,7 @@
+function react () {
+    var element=document.getElementById('send');
+    element.value = "Sorry, I cannot send";
+}
+function out() {
+    document.getElementById('send').value = "Now, I send, thanks!";
+}
